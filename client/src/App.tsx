b96621474c1e4884dd9e04d79b06c4bf30f3c998
Ruslan <110040060+Deadusik@users.noreply.github.com>
2025-02-23 03:03:24 +0200

    update navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import Footbar from "./components/Footbar"
 import Navbar from "./components/Navbar"
 import AppRouter from "./router/AppRouter"
 
-
 function App() {
   return (
     <div className={styles.App}>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Navbar as BootstrapNavbar } from 'react-bootstrap';
+
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <BootstrapNavbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <BootstrapNavbar.Brand href="#home">Social Post</BootstrapNavbar.Brand>
+                <Nav>
+                    <Nav.Link href="#features">Login</Nav.Link>
+                    <Nav.Link href="#pricing">Registration</Nav.Link>
+                </Nav>
+            </Container>
+        </BootstrapNavbar>
     )
 }
 
